a76d7c1b3d037c25c13400381f80df4413696889
Hugo Romero <hugomrj@gmail.com>
2025-06-08 23:42:51 -0400

    junio8

---
+++ app/models/cliente_timbrado_model.py
@@ -0,0 +1,26 @@
+
+from django import forms
+from django.db import models
+from django.apps import apps  
+
+
+
+class ClientesTimbrado(models.Model):
+    id = models.AutoField()
+    clente = models.IntegerField(blank=True, null=True)
+    timbrado = models.CharField(blank=True, null=True)
+    fecha_inicio = models.DateField(blank=True, null=True)
+    fecha_fin = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'clientes_timbrado'
+
+
+
+
+
+class ClientesTimbradoForm(forms.ModelForm):
+    class Meta:
+        model = ClientesTimbrado
+        
